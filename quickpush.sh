4d3c92693bababe6c9e2e85adf9c75d6638f8e9a
Mukund Sudarshan <ms2666@cornell.edu>
2016-03-20 14:17:37 -0400

    pushed code at 14:17:37 on 03/20/2016

--- quickpush.sh
+++ quickpush.sh
@@ -1,4 +1,4 @@
-cd ./
+cd "/Volumes/Macintosh_Extension/Documents_Extension/Google Drive/Cornell/Semester 6/CS 4300/Assignments/Final-Project/cs4300sp2016-party-prediction"
 t=$(date +"%H:%M:%S")
 d=$(date +"%m/%d/%Y")
 git add .
