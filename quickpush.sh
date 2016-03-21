dbf2d20ea0708dd09fe493f626b9963e4afb8b89
Mukund Sudarshan <ms2666@cornell.edu>
2016-03-20 23:57:43 -0400

    improved quickpush script [pushed at 23:57:43 on 03/20/2016]

--- quickpush.sh
+++ quickpush.sh
@@ -1,6 +1,9 @@
+echo "Type a commmit message, followed by [ENTER]:"
+read msg
+
 cd "/Volumes/Macintosh_Extension/Documents_Extension/Google Drive/Cornell/Semester 6/CS 4300/Assignments/Final-Project/cs4300sp2016-party-prediction"
 t=$(date +"%H:%M:%S")
 d=$(date +"%m/%d/%Y")
 git add .
-git commit -m "pushed code at $t on $d"
+git commit -m "$msg [pushed at $t on $d]"
 git push
